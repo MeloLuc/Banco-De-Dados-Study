33b45d0f4f50a184053ecd3b6ea5be803e8c8f37
Lucas De Melo Monteiro Peixoto <lm159753456@gmail.com>
2026-01-29 20:33:03 -0300

    cap 09 - database design

---
+++ oreilly_getting_started_with_sql-master/extra_practice_examples/cap09.sql
@@ -0,0 +1,20 @@
+-- DATABASE DESIGN
+-- 1) What are the business requirements?
+-- 2) What tables will be needed to meet these requirements?
+-- 3) Which column willeach table contain?
+-- 4) How will the tables be normalized?
+-- 5) What will the father/son relationship be like?
+
+-- 1) How many data will be provided in this table?
+-- 2) Who or what will provide the data for the table?
+-- 3) Where will the data come from?
+-- 4) Do we need processes that automatically populate the tables?
+
+-- 1) Who should have acess  to this database?
+-- 2) Who should have access to which table? Read-Only access? Write access?
+-- 3) Is this database critical to business operations?
+-- 4) What backup plan do we have in case of disaster/failure?
+-- 5) Should changes made to the tables be logged?
+-- 6) If the database is used by websites or web applications, is that secure?
+
+-- STRUCTURE OF DATABASE CREATED IN SQLiteStudio ACCORDING WITH THE BOOK
